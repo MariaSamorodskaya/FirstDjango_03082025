a6734d4e77fe5117bd4d140a2ea628621205f168
Maria Samorodskaya <mari_samorodskaya@mail.ru>
2025-08-17 16:47:05 +0300

    Done.M4_1: task3

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from MainApp import views
 
 urlpatterns = [
-    path('', views.main),
-    path('about', views.author),
-    path('item/<int:id_arg>', views.f_items),
-    path('items', views.items_list),
-    path('about', views.author),
+    path('', views.main, name = "home"),
+    path('item/<int:id_arg>', views.f_items, name = "item-detail"),
+    path('items', views.items_list, name = "item-list"),
+    path('about', views.author, name = "author"),
 
 
 
