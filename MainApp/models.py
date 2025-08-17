7c95d3877ca474d8229603a83a081043c97714f9
Maria Samorodskaya <mari_samorodskaya@mail.ru>
2025-08-17 15:16:07 +0300

    Done. M3_2: task2-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,3 +6,4 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100,default="")
     count = models.PositiveIntegerField()
+    description = models.TextField(max_length=400,default="Описание товара")
