92b995a9dd6b6581c2900e8ac47d8a73c0d85230
Maria Samorodskaya <mari_samorodskaya@mail.ru>
2025-08-18 16:24:48 +0300

    Done.M4_1: task1-2

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from MainApp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.main, name = "home"),
     path('item/<int:id_arg>', views.f_items, name = "item-detail"),
     path('items', views.items_list, name = "item-list"),
     path('about', views.author, name = "author"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
-]
+
